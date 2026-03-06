c0bfefc59173afe6fb803b1aa5ca7d7972b879e8
Chanonboth2509 <97206638+Chanonboth2509@users.noreply.github.com>
2026-03-06 14:38:04 +0700

    Delete smart_community directory

--- smart_community/api/check_auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header("Content-Type: application/json");
-if (isset($_SESSION['admin_id'])) {
-    echo json_encode(["auth" => true, "user" => $_SESSION['admin_name']]);
-} else {
-    echo json_encode(["auth" => false]);
-}
-?>
